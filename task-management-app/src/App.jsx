f786d7b53ee7a61d9e1495d67fbe4987cc0406c2
arashj <arashjamshidi12@gmail.com>
2024-06-13 07:17:45 -0400

    Second commit

--- task-management-app/src/App.jsx
+++ task-management-app/src/App.jsx
@@ -1,35 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./Components/Button";
+import NoTaskCreated from "./Components/NoTaskCreated";
+import ProjectSidebar from "./Components/ProjectSidebar";
+import "./index.css";
+import NewTask from "./Components/NewTask";
+import Tasklist from "./Components/Tasklist";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasksState, setTasksState] = useState({
+    selectedTaskId: undefined,
+    tasks: [],
+  });
+
+  const [filter, setFilter] = useState("all");
+
+  const filteredTasks = tasksState.tasks.filter((task) => {
+    if (filter === "completed") return task.isCompleted;
+    if (filter === "incomplete") return !task.isCompleted;
+    return true; // 'all' or any other value will show all tasks
+  });
+
+  function handleDeleteTask(id) {
+    setTasksState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
+
+  const handleUpdateTask = (
+    id,
+    updateTitle,
+    updateDescription,
+    updateDate,
+    updateIsCompleted
+  ) => {
+    setTasksState((prevState) => ({
+      ...prevState,
+      tasks: prevState.tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            title: updateTitle,
+            description: updateDescription,
+            dueDate: updateDate,
+            isCompleted: updateIsCompleted,
+          };
+        }
+        return task;
+      }),
+    }));
+  };
+
+  function handleAddTask(projectData) {
+    setTasksState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        ...projectData,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        selectedTaskId: "listed",
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  function handleCancelAddTask() {
+    setTasksState((prevState) => {
+      return {
+        ...prevState,
+        selectedTaskId: undefined,
+      };
+    });
+  }
+
+  function handleStartAddTask() {
+    setTasksState((prevState) => {
+      return {
+        ...prevState,
+        selectedTaskId: null,
+      };
+    });
+  }
+
+  let content = (
+    <Tasklist
+      tasks={tasksState.tasks}
+      onDeleteTask={handleDeleteTask}
+      onUpdateTask={handleUpdateTask}
+      onAddTask={handleStartAddTask}
+      filteredTasks={filteredTasks}
+    />
+  );
+
+  if (tasksState.selectedTaskId === undefined) {
+    content = <NoTaskCreated onStartAddTask={handleStartAddTask} />;
+  } else if (tasksState.selectedTaskId === null) {
+    content = (
+      <NewTask onAdd={handleAddTask} onCancel={handleCancelAddTask} task="" />
+    );
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectSidebar
+          tasksState={tasksState}
+          setTasksState={setTasksState}
+          setFilter={setFilter}
+        />
+        {content}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
